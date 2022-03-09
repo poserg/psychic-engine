80b6080eef2458486fe2b34db2cfa77ee1b78c1e
Sergei Popov <spopov@cinimex.ru>
2022-03-09 06:43:15 +0300

    Add DatabaseManager

---
+++ DatabaseManager.py
@@ -0,0 +1,40 @@
+import sqlite3
+
+class DatabaseManager:
+	def __init__(self, database_filename):
+		self.connection = sqlite3.connect(database_filename)
+
+	def __del__(self):
+		self.connection.close()
+
+	def _execute(self, statement, values=None):
+		with self.connection:
+			cursor = self.connection.cursor()
+			cursor.execute(statement, values or [])
+			return cursor
+
+	def create_table(self, table_name, columns):
+		columns_with_types = [
+			f'{column_name} {data_type}'
+			for column_name, data_type in columns.items()
+		]
+		self._execute(
+			f'''
+			CREATE TABLE IF NOT EXISTS {table_name}
+			({', '.join(columns_wity_types)});
+			'''
+		)
+
+	def add(self, table_name, data):
+		placeholders = ', '.join('?' * len(data))
+		column_names = ', '.join(data.keys)
+		column_values = tuple(data.values())
+
+		self._execute(
+			f'''
+			INSERT INTO {table_name}
+			({column_values})
+			VALUES ({placeholders});
+			''',
+			column_values
+		)
